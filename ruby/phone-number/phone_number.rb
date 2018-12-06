e4239b7a2c942cc480c79edbb6c3854bb11c2ec0
Krisztina Kovari <krystonen@gmail.com>
2018-12-06 12:52:34 +0100

    Updated solution to phone-number

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,11 +2,10 @@
 # where N is any digit from 2 through 9 and X is any digit from 0 through 9.
 class PhoneNumber
   def self.clean(phone_number)
-    clean_number = phone_number.gsub(/\D/, '')
-    size = clean_number.length
-    regex = /^(\D*[2-9]\d{2}|\D*[1]\D*[2-9]\d{2})\D*[2-9]\d{2}\D*\d{4}\D*$/
-
-    return clean_number if phone_number =~ regex && size == 10
-    return clean_number[1..-1] if phone_number =~ regex && size == 11
+    regex = /^(?:\D*1?)(?:\D*)([2-9]\d{2}) # (NXX)
+            (?:\D*)([2-9]\d{2}) # NXX
+            (?:\D*)(\d{4})(?:\D*)$/x # XXXX
+    r = regex.match(phone_number)
+    r.captures.join unless r.nil?
   end
 end
